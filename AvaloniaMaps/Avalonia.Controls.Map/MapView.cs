78c07e6a2968aa0025e43687a127c6e4ea0c5d40
Gogs <gogs@fake.local>
2022-05-11 15:15:50 +0300

    Add map mouse drag

--- AvaloniaMaps/Avalonia.Controls.Map/MapView.cs
+++ AvaloniaMaps/Avalonia.Controls.Map/MapView.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Media;
 
 namespace AvaloniaMaps.Avalonia.Controls.Map;
@@ -9,13 +10,13 @@ public class MapView : Panel
     public MapRender MapRender { get; }
 
     // TODO: Move these properties to MapControl
-    
+
     public int Level
     {
         get => GetValue(LevelProperty);
         set => SetValue(LevelProperty, value);
     }
-    
+
     public MapPoint ViewCenter
     {
         get => GetValue(ViewCenterProperty);
@@ -48,45 +49,63 @@ public class MapView : Panel
         AvaloniaProperty.RegisterAttached<MapView, MapPoint>(
             "MapPosition", typeof(MapView), MapPoint.Zero);
 
-    private bool _boundsSet = false;
+    private Point _previousMousePos; // Previous mouse position to calculate drag delta
+    private bool _isDragging; // Whether user is currently dragging map
+
     static MapView()
     {
         AffectsRender<MapView>(BoundsProperty);
         AffectsRender<MapView>(LevelProperty);
         AffectsRender<MapView>(ViewCenterProperty);
-        
+
         AffectsArrange<MapView>(BoundsProperty);
         AffectsArrange<MapView>(MapPositionProperty);
         AffectsArrange<MapView>(ViewCenterProperty);
     }
-    
+
     public MapView()
     {
         MapRender = new MapRender();
-        
-        // Update camera view bound on screen resize
-        // TODO: Check why this invokes like 10 times on startup...
-        BoundsProperty.Changed.Subscribe(() =>
-        {
-            MapRender.MapCamera.ViewBounds = Bounds;
 
-            // We need to set ViewCenter after bounds set first time
-            if (_boundsSet || Bounds.Width == 0)
-                return;
-            
-            MapRender.MapCamera.ViewCenter = ViewCenter; // Doesn't work...
-            _boundsSet = true;
-        });
+        // Update camera view bound on screen resize
+        BoundsProperty.Changed.Subscribe(() => { MapRender.MapCamera.ViewBounds = Bounds; });
+
+        PointerMoved += PointerMovedHandler;
+
+        MapRender.MapCamera.OnGenerationFinished += InvalidateVisual;
+    }
+
+    private void PointerMovedHandler(object sender, PointerEventArgs e)
+    {
+        // Start drag if left mouse is held
+        if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            _isDragging = false;
+            return;
+        }
+        var pos = e.GetPosition(this);
+
+        // Init dragging
+        if (!_isDragging)
+        {
+            _previousMousePos = pos;
+            _isDragging = true;
+        }
+
+        MapRender.MapCamera.Translate(pos - _previousMousePos);
+
+        _previousMousePos = pos;
     }
 
     protected override Size ArrangeOverride(Size finalSize)
     {
         foreach (var child in Children)
         {
-            var pos = MapRender.MapCamera.WorldPointToScreenCoordinates(GetMapPosition(child));
-            child.Arrange(new Rect(pos, child.DesiredSize));
+            var childMapPos = GetMapPosition(child);
+            var childScreenPos = MapRender.MapCamera.WorldPointToScreenCoordinates(childMapPos);
+            child.Arrange(new Rect(childScreenPos, child.DesiredSize));
         }
-        
+
         return finalSize;
     }
 
